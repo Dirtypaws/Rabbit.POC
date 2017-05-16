5c71a3d5bbcd93cdaa2b3f3ebc2b124765aec3c7
Matt Krizanac <Matt Krizanac>
2017-05-16 00:54:45 -0500

    Initial commit

---
+++ Consumer/Program.cs
@@ -0,0 +1,49 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "test.qu",
+                                     durable: true,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                channel.BasicQos(0, 1, false);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine(" [x] Received {0}", message);
+                    Console.ResetColor();
+
+                    channel.BasicAck(ea.DeliveryTag, false);
+                };
+
+                channel.BasicConsume(queue: "test.qu",
+                                     noAck: false,
+                                     consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Publisher/Program.cs
@@ -0,0 +1,78 @@
+﻿using CommandLine;
+using CommandLine.Text;
+using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace Publisher
+{
+    class Program
+    {
+        private class Options
+        {
+            [Option('f', "fill", Required = false, HelpText = "Fills the queue with the specified amount of messages")]
+            public int Fill { get; set; }
+
+            [HelpOption]
+            public string GetUsage()
+            {
+                return HelpText.AutoBuild(this, current => HelpText.DefaultParsingErrorsHandler(this, current));
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var opts = new Options();
+            if(Parser.Default.ParseArguments(args, opts))
+            {
+                for (int i = 0; i < opts.Fill; i++)
+                {
+                    var now = DateTime.Now.ToString("ddMMyyyy hh:mm:ss");
+                    Publish($"{i} - {now}");
+                }
+            }
+            else
+            {
+                Listen();
+            }
+        }
+
+        private static void Listen()
+        {
+            Console.Write("> ");
+            var msg = Console.ReadLine();
+            Publish(msg);
+            Listen();
+        }
+
+        private static void Publish(string msg)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "test.qu",
+                     durable: true,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+
+                var properties = channel.CreateBasicProperties();
+                properties.Persistent = true;
+                
+                var body = Encoding.UTF8.GetBytes(msg);
+
+                channel.BasicPublish(exchange: "",
+                                        routingKey: "test.qu",
+                                        basicProperties: properties,
+                                        body: body);
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine(" Publisher Sent {0}", msg);
+                Console.ResetColor();
+            }
+        }
+
+        
+    }
+}
